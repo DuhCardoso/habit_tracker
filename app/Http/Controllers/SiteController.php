4c73e4738d1f3bcdd5c27a38f494f0e88d330d84
Eduardo Cardoso de J. <94879340+DuhCardoso@users.noreply.github.com>
2026-02-10 15:48:04 -0300

    init commit

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        $name = "Eduardo";
+        $habits = ["Correr", "Ler", "Codar"];
+        return view('home', compact('name', 'habits'));
+    }
+}
